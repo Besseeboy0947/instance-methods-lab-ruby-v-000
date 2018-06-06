d867c8d53ed8266fd1efdf91f7154a0896a660fe
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-06-06 17:50:56 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def bark
+    puts "Woof!"
+  end
+end
+ 
+fido = Dog.new
+fido.bark #> "Woof!"
+ 
+snoopy = Dog.new
+snoopy.bark #> "Woof!"
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+ 
