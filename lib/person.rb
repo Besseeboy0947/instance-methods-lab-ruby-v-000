be54ac403a247d019ccbd4c339a2975031b32b24
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-06-06 17:55:36 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,18 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+end
+ 
+fido = Person.new
+fido.bark #> "Hello World!"
+ 
+snoopy = Person.new
+snoopy.bark #> "The Person is walking"
 
+class Person
+  def walk
+    puts "The Person is walking"
+  end
+end
  
